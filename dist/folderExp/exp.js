86cec755273cecae24af41c9acfbd233a1214e21
RyanLaiber <ryanlaiber@gmail.com>
2021-12-14 14:45:18 -0300

    aprendendo opçoes de typescript

---
+++ dist/folderExp/exp.js
@@ -0,0 +1,6 @@
+"use strict";
+const numb = 5;
+function logg(n1) {
+    console.log(n1);
+}
+logg(numb);
---
+++ src/folderExp/exp.ts
@@ -0,0 +1,7 @@
+const numb = 5;
+
+function logg(n1: number): void {
+  console.log(n1);
+}
+
+logg(numb);
